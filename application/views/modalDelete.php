59e142d7828bf78042a8688b3cfbc1b4b08f88d2
sulyhc <skydestroyer99@gmail.com>
2016-06-08 11:49:18 -0700

    adding file of deleteModal

---
+++ application/views/modalDelete.php
@@ -0,0 +1,24 @@
+<div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">SOLICITANDO CONFIRMACION</h4>
+      </div>
+      <div class="modal-body">
+        <div class="row">
+        	<div class="col-md-3">
+        		<img src="<?php echo base_url("assets/img/warning.png") ?>" />
+        	</div>
+        	<div class="col-md-9">
+        		<h2 style="color: red">DESEA ELIMINAR EL ELEMENTO SELECCIONADO?</h2>
+        	</div>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+        <button type="button" class="btn btn-success" data-dismiss="modal">Agregar</button>
+      </div>
+    </div>
+  </div>
